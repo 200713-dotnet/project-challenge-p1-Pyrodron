428cf8ca2dea1a514ae69a8c4fa00e5231feda5f
kyle dick <kyle.i.dick@outlook.com>
2020-08-06 21:50:45 -0400

    Order History page shows topping names instead of their IDs

--- PizzaStore.Client/Controllers/OrderController.cs
+++ PizzaStore.Client/Controllers/OrderController.cs
@@ -33,14 +33,25 @@ namespace PizzaStore.Client.Controllers {
 
       List<OrderViewClass> orderHistory = new List<OrderViewClass>();
       foreach (OrderModel order in orders) {
-        PizzaModel pizza = _db.Pizzas.Where(p => p.ID == order.PizzaID).SingleOrDefault();
+        StringBuilder toppings = new StringBuilder();
+        foreach (string topping in order.Toppings.Split(',')) {
+          int toppingID;
+          if (!int.TryParse(topping, out toppingID)) {
+            Console.WriteLine($"Database error: Expected integer for pizza ID, received {topping}");
+            toppings.Append("Error, ");
+            continue;
+          }
+          ToppingModel top = _db.Toppings.Where(t => t.ID == toppingID).SingleOrDefault();
+          toppings.Append($"{top.Name}, ");
+        }
+        toppings.Remove(toppings.Length - 2, 2);
         orderHistory.Add(new OrderViewClass{
           OrderID = order.OrderID,
           Created = order.Created,
-          Pizzas = pizza.ToString(),
+          Pizzas = _db.Pizzas.Where(p => p.ID == order.PizzaID).SingleOrDefault().Name,
           Size = order.Size,
           Crust = _db.Crust.Where(c => c.ID == order.CrustID).SingleOrDefault().Name,
-          Toppings = order.Toppings,
+          Toppings = toppings.ToString(),
           Quantity = order.Quantity,
           Cost = order.TotalCost,
           StoreName = _db.Stores.Where(s => s.ID == order.StoreID).SingleOrDefault().Name
